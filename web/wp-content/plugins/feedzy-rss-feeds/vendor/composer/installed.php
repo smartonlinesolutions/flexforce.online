8a6e753c326502cdb4fbc6983c9a7dadd23a4f8a
smartonlinesolutions <info@smartonlinesolutions.nl>
2023-07-13 08:41:56 +0200

    added plugin

---
+++ web/wp-content/plugins/feedzy-rss-feeds/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/feedzy-rss-feeds',
+        'pretty_version' => 'v4.2.6',
+        'version' => '4.2.6.0',
+        'reference' => '669dc474ff953065ed9bb9c9de2a877b2a7f9f9d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/feedzy-rss-feeds' => array(
+            'pretty_version' => 'v4.2.6',
+            'version' => '4.2.6.0',
+            'reference' => '669dc474ff953065ed9bb9c9de2a877b2a7f9f9d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.0',
+            'version' => '3.3.0.0',
+            'reference' => '68dc5d11c1d7a20a13f3ae730183f61ff309d574',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
